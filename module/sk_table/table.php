8a0f9407c7ee64a0a51874130bdcff4795151057
hifi <github@rosenhahn.de>
2020-03-01 10:14:26 +0100

    sk_table 1.8 - added max-width styling

--- module/sk_table/table.php
+++ module/sk_table/table.php
@@ -25,7 +25,7 @@ function build_table($array, $buttonvalue = 'toggle table'){
   $html .= '<thead><tr>';
   $i = "0";
   foreach ($array[0] as $key => $value) {
-    $html .= '<th onclick="sortTable(' . $i . ')">'.htmlspecialchars($key).'</th>';
+    $html .= '<th onclick="sortTable(' . $i . ')">' . htmlspecialchars($key) . '</th>';
     $i++;
   }
   $html .= '</tr></thead>';
@@ -47,17 +47,17 @@ function build_table_swap($array){
   $html .= '<table id="myTable">';
   $html .= '<thead><tr>';
   $i = "0";
-  foreach($array as $key=>$value){
-    $html .= '<th onclick="sortTable('.$i.')">'.htmlspecialchars($key).'</th>';
+  foreach($array as $key => $value){
+    $html .= '<th onclick="sortTable(' . $i . ')">' . htmlspecialchars($key) . '</th>';
     $count[$i] = count($value);
     $i++;
   }
   $html .= '</tr></thead>';
 
-  for($i="0";$i<$count[0];$i++){
+  for ($i = "0"; $i < $count[0]; $i++) {
     $html .= '<tr>';
-    foreach($array as $key=>$value){
-      $html .= '<td>'.$value[$i].'</td>';
+    foreach ($array as $key => $value) {
+      $html .= '<td>' . $value[$i] . '</td>';
     }
     $html .= '</tr>';
   }
